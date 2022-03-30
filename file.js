11b61052e38941e679d49a9af2b0cb876a8e6277
William-95 <101137872+William-95@users.noreply.github.com>
2022-03-30 15:15:11 +0200

    .

--- file.js
+++ file.js
@@ -1,12 +1,12 @@
-function sendMail(params){
-  let tempParams ={
-    user_email:document.getElementById("user_email").value,
-    user_name:document.getElementById("user_name").value,
-    message:document.getElementById("message").value
+function sendMail(params) {
+  let tempParams = {
+    user_email: document.getElementById("user_email").value,
+    user_name: document.getElementById("user_name").value,
+    message: document.getElementById("message").value
   };
 
-  emailjs.send("service_i1mfj9b","template_0d85kaf",tempParams)
-  .then(function(res){
-    console.log("success",res.status);
-  });
+  emailjs.send("service_i1mfj9b", "template_0d85kaf", tempParams)
+    .then(function(res) {
+      console.log("success", res.status);
+    });
 };
